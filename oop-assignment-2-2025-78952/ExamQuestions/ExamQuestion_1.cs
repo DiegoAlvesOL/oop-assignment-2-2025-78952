7f32950bc2ccf7b8deb876915135d8bd006da7fc
Diego Alves de Oliveira <diego@Diegos-MacBook-Air-2.local>
2025-12-11 15:45:20 +0000

    new commit

---
+++ oop-assignment-2-2025-78952/ExamQuestions/ExamQuestion_1.cs
@@ -0,0 +1,6 @@
+namespace oop_assignment_2_2025_78952.ExamQuestions;
+
+public class ExamQuestion_1
+{
+    
+}
--- oop-assignment-2-2025-78952/Models/Q1A_MobileValidator.cs
+++ oop-assignment-2-2025-78952/Models/Q1A_MobileValidator.cs
@@ -4,10 +4,7 @@ namespace oop_assignment_2_2025_78952.Models
 {
     public class Q1A_MobileValidator
     {
-        // O regex:
-        // ^(083|085|089) - Começa com 083, 085 ou 089
-        // \d{7} - Seguido por exatamente 7 dígitos (totalizando 10)
-        // $ - Termina (garante que não há mais caracteres, como espaços ou símbolos)
+        
         private const string MobileRegexPattern = @"^(083|085|089)\d{7}$";
 
         public bool IsValidIrishMobile(string number)
@@ -17,7 +14,6 @@ namespace oop_assignment_2_2025_78952.Models
                 return false;
             }
             
-            // Verifica se a string corresponde exatamente ao padrão Regex
             return Regex.IsMatch(number, MobileRegexPattern);
         }
         
